3b4055dfe534ec28a2a8ed4585ab6437e35586c4
raz zalach <raz62015@gmail.com>
2022-11-26 20:32:08 +0200

    make it better2

--- api/v1/routes/membership.js
+++ api/v1/routes/membership.js
@@ -1,5 +1,5 @@
 const router=require('express').Router();
-const {Register,Login,Get_All_Active,Get_All_Active2,DeleteMember}=require('../controller/MemberShip');
+const {Register,Login,Get_All_Active,Get_All_Active2,DeleteMember}=require('../controller/membership.js');
 router.post("/reg",Register);
 router.post("/log",Login);
 router.get("/del/:Email",DeleteMember);
